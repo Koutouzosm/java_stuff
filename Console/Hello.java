ecb0786ca1b21bf6b9c0ca2d8a0047c82850968b
Mano <Koutouzosm@gmail.com>
2020-06-24 20:33:19 -0400

    added notes for referance

--- Console/Hello.java
+++ Console/Hello.java
@@ -7,5 +7,9 @@ public class Hello {
         // To print and run file java Hello.java or java -cp(classpath) Console.Hello...(path)
         // shortcut for compiling java and fileName
         System.out.println("Hello World");
+
+        // Primative type variables include String, char(one letter in string), Int, long, short, byte, float(Decimal point), double(Decimal point but more accurate more widely used), boolean
+
     }
 }
+
