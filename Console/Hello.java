9e06b5688ca5502c60cf60a5ebf8e7a060fb3ed2
Mano <Koutouzosm@gmail.com>
2020-06-24 20:17:17 -0400

    add comments for compiling for referance

--- Console/Hello.java
+++ Console/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
     public static void main(String[] args){
         // Printing out my first Hello World 
         // To compile open in terminal javac Hello.java
-        // To print and run file java Hello.java or java -cp Console.Hello...(path)
+        // To print and run file java Hello.java or java -cp(classpath) Console.Hello...(path)
+        // shortcut for compiling java and fileName
         System.out.println("Hello World");
     }
 }
