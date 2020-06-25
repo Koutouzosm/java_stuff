230500274138044afffc7abd1b1e8965887cedce
Mano <Koutouzosm@gmail.com>
2020-06-24 20:08:34 -0400

    My first Hello World in Java

---
+++ Console/Hello.java
@@ -0,0 +1,10 @@
+package Console;
+
+public class Hello {
+    public static void main(String[] args){
+        // Printing out my first Hello World 
+        // To compile open in terminal javac Hello.java
+        // To print and run file java Hello.java or java -cp Console.Hello...(path)
+        System.out.println("Hello World");
+    }
+}
