e1459db6ac446af36e000d18ab7bbe9f8667baa3
Mano <Koutouzosm@gmail.com>
2020-06-24 20:53:03 -0400

    first loop in java

--- Console/Hello.java
+++ Console/Hello.java
@@ -1,14 +1,23 @@
 package Console;
 
-public class Hello {
+import java.util.Scanner;
+
+class Hello {
     public static void main(String[] args){
+
+        // private/public is were the method is accesible 
+        // void prints to console this method does not have a return value
         // Printing out my first Hello World 
         // To compile open in terminal javac Hello.java
         // To print and run file java Hello.java or java -cp(classpath) Console.Hello...(path)
         // shortcut for compiling java and fileName
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
 
         // Primative type variables include String, char(one letter in string), Int, long, short, byte, float(Decimal point), double(Decimal point but more accurate more widely used), boolean
+
+        // Setting a String variable
+        String helloWorld = "Hello World";
+        System.out.println(helloWorld);
 
     }
 }
---
+++ Console/Loop.java
@@ -0,0 +1,9 @@
+package Console;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
